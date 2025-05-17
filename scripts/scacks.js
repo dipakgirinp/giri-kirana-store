5536f3c145cdbef1629f51beea9f8bdb616a7e3e
dipakgirinp <giridipak070@gmail.com>
2025-05-17 14:12:13 +0545

    description
    dfadfadf

--- scripts/scacks.js
+++ scripts/scacks.js
@@ -15,24 +15,5 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-//only in mobile
-document.addEventListener('DOMContentLoaded', () => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    const isMobile = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(userAgent);
-  
-    if (!isMobile) {
-        document.body.innerHTML = `
-          <style>
-           body{
-            background:black;
-           }
-          </style> 
-         <h1 style="text-align:center; margin-top:200px; letter-spacing:1px; color:white;">
-             This website is only available on mobile devices <br><br>
-             <span style="font-size:0.9rem;">( try again in a mobile device )</span>
-         </h1>
-        `;
-    }
-  });
 
 
